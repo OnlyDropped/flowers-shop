4c933afe0c397de9a87f5084a12e87415faefddd
OnlyDropped <asheburenkov@yandex.ru>
2022-10-25 20:14:41 +0300

    added new changes

--- js/app.js
+++ js/app.js
@@ -112,32 +112,10 @@ $('.reviews-slider').slick({
       breakpoint: 590,
       settings: {
         slidesToShow: 1,
-        slidesToScroll: 1,
       }
     },
-    {
-      breakpoint: 415,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-      }
-    },
-    {
-      breakpoint: 376,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-      }
-    }
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
   ]
 })
-
-
-
-
 
 
 
